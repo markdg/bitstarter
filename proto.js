85476d46effa527779ccda6fa395f8994a38727a
Ubuntu <ubuntu@ip-172-31-24-204.us-west-2.compute.internal>
2013-08-29 06:32:52 +0000

    Updates

--- proto.js
+++ proto.js
@@ -23,15 +23,48 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 
-//=== markdg-bitstarter-s-mooc Config Vars from heroku cli inquiry
+
+//===db stuff===
+
+//markdg-bitstarter-s-mooc Config Vars from heroku cli inquiry
 //MONGOHQ_URL: mongodb://heroku:366acce9fd196a673584b13acb417573@dharma.mongohq.com:10091/app17007481
+//MONGOLAB_URI: mongodb://heroku_app17007481:ibp5bs8on9kgn8e6j07q8l4e86@ds037478.mongolab.com:37478/heroku_app17007481
 //PATH:        bin:node_modules/.bin:/usr/local/bin:/usr/bin:/bin
 
-var MONGOHQ_URL = 'mongodb://heroku:366acce9fd196a673584b13acb417573@dharma.mongohq.com:10091/app17007481'
-
-var itineraryProvider = new ItineraryProvider(MONGOHQ_URL, 27017);
+//all of these have been tried and don't work
+//var MONGOHQ_URL = ' mongodb://heroku:5953c7c83dcadf88b07ecf0605a5f71d@dharma.mongohq.com:10091/app17007481';
+//var itineraryProvider = new ItineraryProvider(MONGOHQ_URL, 10091);
 //var itineraryProvider = new ItineraryProvider(process.env.MONGOHQ_URL, 27017);
-//var itineraryProvider = new ItineraryProvider('localhost', 27017);
+//var MONGOLAB_URI = 'mongodb://heroku_app17007481:ibp5bs8on9kgn8e6j07q8l4e86@ds037478.mongolab.com:37478/heroku_app17007481';
+//var MONGOLAB_HOST = 'ds037478.mongolab.com';
+//var itineraryProvider = new ItineraryProvider(MONGOLAB_HOST, 37478);
+
+//the following works both on my local machine and my EC2 instance but not heroku
+var itineraryProvider = new ItineraryProvider('localhost', 27017);
+
+//=== end db stuff
+
+//db test page
+app.get('/db', function(req, res){
+  res.render('db');
+});
+//db test connection just outputs a message
+app.get('/dbconnect', function(req, res){
+  console.log('test db connection');
+  res.redirect('db');
+});
+//invoke the getCollection method
+app.get('/getCollection', function(req, res){
+  itineraryProvider.getCollection();
+  res.redirect('db');
+});
+//invoke the findAll method
+app.get('/findAll', function(req, res){
+  itineraryProvider.findAll();
+  res.redirect('db');
+});
+//===
+
 
 //render the bitstarter page
 app.get('/', function(req, res){
